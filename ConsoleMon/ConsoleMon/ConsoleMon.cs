6b5eaf2cc36446224a9ce33bcd34c6653eb5e579
Mitchel Klijn <mitchelklijn2004@gmail.com>
2022-05-23 11:56:28 +0200

    opdrachten t/m constructors

---
+++ ConsoleMon/ConsoleMon/ConsoleMon.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleMon
+{
+    internal enum Elementen
+    {
+        Lucht,
+        Aarde,
+        Vuur,
+        Water
+    }
+
+    class ConsoleMon
+    {
+        List<Skill> skills = new List<Skill>();
+
+        Elementen weakness = Elementen.Water;
+
+        internal int health;
+        internal int energy;
+        internal string name;
+
+        internal ConsoleMon()
+        {
+
+        }
+
+        internal ConsoleMon(ConsoleMon copyFrom)
+        {
+            this.health = copyFrom.health;
+            this.energy = copyFrom.energy;
+            this.name = copyFrom.name;
+            this.skills = copyFrom.skills;
+
+            for(int i = 0; i < copyFrom.skills.Count; i++)
+            {
+                Skill copyFromThisSkill = copyFrom.skills[i];
+                Skill clone = new Skill(copyFromThisSkill);
+                this.skills.Add(clone);
+            }
+        }
+
+        internal void TakeDamage(int damage)
+        {
+            health -= damage;
+        }
+
+        internal void DepleteEnergy(int energy)
+        {
+            this.energy -= energy;
+        }
+    }
+}
---
+++ ConsoleMon/ConsoleMon/Skill.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleMon
+{
+    internal class Skill
+    {
+        Elementen element = Elementen.Vuur;
+
+        int damage;
+        int energyCost;
+        string name;
+
+        internal Skill()
+        {
+
+        }
+
+        internal Skill(Skill copyFrom)
+        {
+            this.damage = copyFrom.damage;
+            this.energyCost = copyFrom.energyCost;
+            this.name = copyFrom.name;
+        }
+
+        void UseOn(ConsoleMon target, ConsoleMon caster)
+        {
+            caster.DepleteEnergy(energyCost);
+            target.TakeDamage(damage);
+        }
+    }
+}
